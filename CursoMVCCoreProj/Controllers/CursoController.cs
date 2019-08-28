204fcdb571f5da1256d4b7a98567fc5542f2db46
Antonio Carlos da Silva <acdasilva@gmail.com>
2019-08-28 20:22:27 -0300

    Commit inicial

---
+++ CursoMVCCoreProj/Controllers/CursoController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using CursoMVCCoreProj.Models;
+
+namespace CursoMVCCoreProj.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            Curso objCurso = new Curso { Codigo = 20486, Nome = "Developing MVC Core Web Applications" };
+            return View(objCurso);
+        }
+
+        // GET: Curso/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Curso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Curso/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Curso/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Curso/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Curso/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Curso/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CursoMVCCoreProj/Models/Curso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVCCoreProj.Models
+{
+    public class Curso
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
